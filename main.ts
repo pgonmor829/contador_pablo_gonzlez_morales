38a6d1c787bf1cba3ef4fe626fd5870688425637
pgonmor829 <pgonmor829@g.educaand.es>
2025-01-15 12:18:25 +0100

    corregido

--- main.ts
+++ main.ts
@@ -1,13 +1,12 @@
 input.onButtonPressed(Button.A, function () {
-    contador = 0
+    contador = -1
 })
 let contador = 0
-contador = 0
+contador = -1
 basic.forever(function () {
-    basic.showNumber(contador)
     contador += 1
     if (contador > 20) {
-        contador = 0
+        contador = -1
     }
     basic.pause(1000)
 })
